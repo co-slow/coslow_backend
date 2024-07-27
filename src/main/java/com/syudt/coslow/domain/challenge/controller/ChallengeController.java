5716db530475a9ebc7e6eaf728e9e1fb3112a2e9
sungjun4403 <geulligu89@naver.com>
2024-07-28 03:05:37 +0900

    🚑️ : cors config, mysql connection

--- src/main/java/com/syudt/coslow/domain/challenge/controller/ChallengeController.java
+++ src/main/java/com/syudt/coslow/domain/challenge/controller/ChallengeController.java
@@ -1,7 +1,12 @@
 package com.syudt.coslow.domain.challenge.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ChallengeController {
+    @GetMapping("hello")
+    public String hello () {
+        return "Helloooooo";
+    }
 }
